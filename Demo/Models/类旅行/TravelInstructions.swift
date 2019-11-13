e155a457a10794669d8a1a2ee832cb6b4a28c95f
wangyongyue <wangyouyue@kingbirdplus.com>
2019-11-13 18:31:18 +0800

    m

--- Demo/Models/类旅行/TravelInstructions.swift
+++ Demo/Models/类旅行/TravelInstructions.swift
@@ -14,10 +14,10 @@ class TravelInstructions: Configuration {
          
       let tab = BaseTabBarController()
       tab.addChildVC(childVC: TraHome().v_viewController(), childTitle: "首页", image: Image.home(), selectedImage: Image.home_sel())
-      tab.addChildVC(childVC: TraTrip().v_viewController(), childTitle: "同城", image: Image.home(), selectedImage: Image.home_sel())
-      tab.addChildVC(childVC: TraJourney().v_viewController(), childTitle: "消息", image: Image.home(), selectedImage: Image.home_sel())
-      tab.addChildVC(childVC: TraService().v_viewController(), childTitle: "我", image: Image.home(), selectedImage: Image.home_sel())
-        tab.addChildVC(childVC: TraMine().v_viewController(), childTitle: "我", image: Image.home(), selectedImage: Image.home_sel())
+      tab.addChildVC(childVC: TraTrip().v_viewController(), childTitle: "行程", image: Image.home(), selectedImage: Image.home_sel())
+      tab.addChildVC(childVC: TraJourney().v_viewController(), childTitle: "旅拍", image: Image.home(), selectedImage: Image.home_sel())
+      tab.addChildVC(childVC: TraService().v_viewController(), childTitle: "客服", image: Image.home(), selectedImage: Image.home_sel())
+        tab.addChildVC(childVC: TraMine().v_viewController(), childTitle: "我的", image: Image.home(), selectedImage: Image.home_sel())
 
       return tab
      }
